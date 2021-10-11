d8d9cb2bf4277a25edea927f77c8c2d2b350f2be
Manos Fregkos <manos.fregkos@datawise.ai>
2021-10-11 14:23:46 +0300

    adding products-sagas

--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,8 +1,9 @@
 import { all, call } from "redux-saga/effects";
 import userSagas from "./User/user.sagas";
+import productsSagas from "./products/products.sagas";
 // {all} mas epitrepei na kanoume Promise.all (dld parallhla na ginontai resolve ola ta saga)
 // {call} enw me tin call apla kaloume ta functions
 
 export default function* rootSaga() {
-  yield all([call(userSagas)]);
+  yield all([call(userSagas), call(productsSagas)]);
 }
